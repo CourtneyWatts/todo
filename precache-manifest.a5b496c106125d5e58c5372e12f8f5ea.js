6a4682c4daa2ca377d6e048134dc7d994d9d4bb9
Courtney-Watts <courtney@siteglide.com>
2020-04-03 16:23:53 +0100

    Updates

---
+++ precache-manifest.a5b496c106125d5e58c5372e12f8f5ea.js
@@ -0,0 +1,38 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "a334fa6f64449dcc7a41024174748f9e",
+    "url": "/todo/index.html"
+  },
+  {
+    "revision": "22657f9687fb807ea403",
+    "url": "/todo/static/css/main.e94631e6.chunk.css"
+  },
+  {
+    "revision": "b3d265fd31cd8cdf2152",
+    "url": "/todo/static/js/2.f908d568.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/todo/static/js/2.f908d568.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "22657f9687fb807ea403",
+    "url": "/todo/static/js/main.4129e8ea.chunk.js"
+  },
+  {
+    "revision": "10fd3631c43d335a3db9",
+    "url": "/todo/static/js/runtime-main.2a8ec137.js"
+  },
+  {
+    "revision": "5360a8890322d98a391a86c46b100522",
+    "url": "/todo/static/media/add-button.5360a889.svg"
+  },
+  {
+    "revision": "c7e6fe431e4c35d2303d71046019921d",
+    "url": "/todo/static/media/delete.c7e6fe43.svg"
+  },
+  {
+    "revision": "014be509677f96b6fd6c48efe6a01377",
+    "url": "/todo/static/media/edit.014be509.svg"
+  }
+]);
